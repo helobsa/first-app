f1b49f6487c3f8d0b5bebb5944d9ab2e7325d46d
helobsa <helobsa@alu.ufc.br>
2021-08-31 17:31:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ const { urlencoded } = require("express")
 const port = 3000;
 const address = "localhost";
 
+const utils = require("./utils");
+
+const faker = require ("faker");
+
+let toggleBol = true;
+
+global.users =[];
+
+for (let cont=0;cont<20;cont++){
+    users.push({name:faker.name.findName(),email:faker.internet.email(),address:faker.address.streetAddress(),age:utils.getRandomByInterval(15,50,true),heigth:utils.getRandomByInterval(1.50,1.70,false).toFixed(2),vote:toggleBol});
+    toggleBol=!toggleBol;
+}
+
 app.use(routes);
+
+app.set('view engine','ejs');
+app.use(expressLayouts);
+app.use(express.urlencoded({extended:false})); 
+app.use(express.json());
+
+//app.use(utils.delay(2000));
+
+app.use('/',routes);
 
 const server = app.listen(port, address,()=> {
     let host = server.address().address;
