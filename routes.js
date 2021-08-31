ad11f7abea9684cedba97f6361e2dbb28a162539
helobsa <helobsa@alu.ufc.br>
2021-08-31 13:40:06 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const faker = require("faker");
+const { isModuleNamespaceObject } = require("util/types");
 
 let db = require("./db");
+
+router.get('/', (req, res)=>{
+res.render('pages/home');
+});
+
+module.exports = router;
