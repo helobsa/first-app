2d065f3a8f20693ab7cca33dcf027cadce9910d4
helobsa <helobsa@alu.ufc.br>
2021-08-31 13:40:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require ("express");
-const app = express ();
+const express = require("express");
+const app = express();
+const routes = require("./routes");
 
 const port = 3000;
 const address = "localhost";
+
+app.use(routes);
+
 const server = app.listen(port, address,()=> {
     let host = server.address().address;
     let port = server.address().port;
